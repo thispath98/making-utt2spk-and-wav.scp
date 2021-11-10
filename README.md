437826ea853ef048452c1db72c6bdad0ec981f10
thispath98 <l0641@naver.com>
2021-11-10 16:14:38 +0900

    small modify

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # Files making `utt2spk` & `wav.scp`
-+ `USER_NAME`에 본인 우분투 user 입력
-+ `TYPE`에 `train` 또는 `test 입력
+
+## 1. 환경
+디렉토리 `sample` 안에 `train` 디렉토리와 `test` 디렉토리가 존재함.
+
+---
+
+## 2. 실행 방법
+`DATA_DIR`에 샘플에 대한 경로를 적어줌.
+> e.g. DATA_DIR = "/home/thispath/sample/
+
+```Shell
+python3 make_utt2spk.py
+python3 make_wav.scp.py
+```
+
+---
+## 3. 결과
+sample 디렉토리 내의 train, test 디렉토리 안에 각각 `utt2spk`, `wav.scp`가 생성됨
--- make_utt2spk.py
+++ make_utt2spk.py
@@ -1,18 +1,19 @@
 import os
-USER_NAME = ""
-TYPE = ""
+TYPE = ["train", "test"]
+DATA_DIR = ""
 
-url = f"/home/{USER_NAME}/sample/{TYPE}/"
-dir_list = os.listdir(url)
+for type in TYPE:
+    PATH = f"{DATA_DIR}/{type}/"
+    dir_list = os.listdir(PATH)
 
-with open(url + "utt2spk", 'w') as f:
-    for _dir in dir_list:
-        if not os.path.isdir(url + _dir):
-            continue
-        wav_list = os.listdir(url + _dir + "/")
-        for wav in wav_list:
-            if wav.endswith(".wav"):
-                name = wav.rstrip(".wav")
-                utterance_id = _dir + "_" + name
-                speaker_id = _dir
-                f.write(" ".join([utterance_id, speaker_id]) + "\n")
+    with open(PATH + "utt2spk", 'w') as f:
+        for _dir in dir_list:
+            if not os.path.isdir(PATH + _dir):
+                continue
+            wav_list = os.listdir(PATH + _dir + "/")
+            for wav in wav_list:
+                if wav.endswith(".wav"):
+                    name = wav.rstrip(".wav")
+                    utterance_id = _dir + "_" + name
+                    speaker_id = _dir
+                    f.write(" ".join([utterance_id, speaker_id]) + "\n")
