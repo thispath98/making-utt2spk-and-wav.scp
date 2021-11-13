8b76b9e656a69c184aba4ede7c5242d478ee2d75
thispath98 <l0641@naver.com>
2021-11-13 13:28:48 +0900

    small refactor

--- make_utt2spk_and_wav.scp.py
+++ make_utt2spk_and_wav.scp.py
@@ -1,55 +1,51 @@
 import os
 import argparse
 
-def main():
-    parser = argparse.ArgumentParser()
-    
-    # Required parameter
-    parser.add_argument(
-            "--data_dir",
-            default=None,
-            type=str,
-            required=True,
-            help="directory containing sample data"
-        )
+TYPE = ["train", "test"]
+FILES = ["wav.scp", "utt2spk"]
 
+
+def main():
+    # Required parameter
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data_dir", default=None, type=str,
+                        required=True, help="directory containing sample data")
     args = parser.parse_args()
-    
-    TYPE = ["train", "test"]
-    FILES = ["wav.scp", "utt2spk"]
 
     for type in TYPE:
-        PATH = "{}/{}/".format(args.data_dir, type)
+        PATH = os.path.join(args.data_dir, type)
         dir_list = os.listdir(PATH)
 
         print("Making {} in {} directory...".format(FILES[0], type))
-        with open(PATH + FILES[0], 'w') as f:
-            for _dir in dir_list:
-                if not os.path.isdir(PATH + _dir):
+        with open(os.path.join(PATH, FILES[0]), 'w') as f:
+            for directory in dir_list:
+                target = os.path.join(PATH, directory)
+                if not os.path.isdir(target):
                     continue
-                wav_list = os.listdir(PATH + _dir + "/")
+
+                wav_list = os.listdir(target + "/")
                 for wav in wav_list:
                     if wav.endswith(".wav"):
-                        name = wav.rstrip(".wav")
-                        utterance_id = _dir + "_" + name + " "
-                        wav_path = PATH + _dir + "/" + wav
-                        f.write(" ".join([utterance_id + wav_path]) + "\n")
+                        utterance_id = directory + "_" + wav.rstrip(".wav")
+                        wav_path = target + "/" + wav
+                        f.write(" ".join([utterance_id, wav_path]) + "\n")
         print("done")
 
         print("Making {} in {} directory...".format(FILES[1], type))
-        with open(PATH + FILES[1], 'w') as f:
-            for _dir in dir_list:
-                if not os.path.isdir(PATH + _dir):
+        with open(os.path.join(PATH, FILES[1]), 'w') as f:
+            for directory in dir_list:
+                target = os.path.join(PATH, directory)
+                if not os.path.isdir(target):
                     continue
-                wav_list = os.listdir(PATH + _dir + "/")
+
+                wav_list = os.listdir(target)
                 for wav in wav_list:
                     if wav.endswith(".wav"):
-                        name = wav.rstrip(".wav")
-                        utterance_id = _dir + "_" + name
-                        speaker_id = _dir
+                        utterance_id = directory + "_" + wav.rstrip(".wav")
+                        speaker_id = directory
                         f.write(" ".join([utterance_id, speaker_id]) + "\n")
         print("done")
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
